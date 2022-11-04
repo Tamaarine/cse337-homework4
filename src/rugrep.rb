6cf6f1db5bc038b97495445e1215ea5d4f2d651f
Tamaarine <irebootplaygt@gmail.com>
2022-11-04 19:54:40 -0400

    Defined: parse_regex which utilizes regex? to return an Regexp object

--- src/rugrep.rb
+++ src/rugrep.rb
@@ -10,6 +10,26 @@ def regex?(exp)
   exp[0] == '"' and exp[-1] == '"' and exp.length != 1
 end
 
+def parse_regex(exp)
+  # Return the regex object that represents the
+  # pattern provided by the input exp.
+  # exp must conform the specified string style, be surrounded
+  # by double quotes.
+  # Return nil if it cannot construct the regex object
+  if not regex?(exp)
+    return nil
+  end
+  
+  begin
+    return Regexp.new(exp[1...-1])
+  rescue RegexpError
+    return nil
+  rescue
+    # Catch all exception
+    return nil
+  end
+end
+
 def parseArgs(args)
   # Fill me
 end
