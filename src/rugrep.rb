71dc634611f01921efa44fd308b44dc65f3a310d
Tamaarine <irebootplaygt@gmail.com>
2022-11-04 19:34:58 -0400

    Defined: regex? Function that will be used as a helper function to identity which is a regex string

--- src/rugrep.rb
+++ src/rugrep.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 args = ARGF.argv
 # args = ARGV
+
+def regex?(exp)
+  # Returns true if the input string is a regex
+  # specified by two quotes in front and the back
+  # ex. \"[ab]pple\" is valid
+  # ex. \" is not valid
+  exp[0] == '"' and exp[-1] == '"' and exp.length != 1
+end
+
 def parseArgs(args)
   # Fill me
 end
