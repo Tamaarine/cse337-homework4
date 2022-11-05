caf2bf64824e23e473063b68716a28b8bfb4985e
Tamaarine <irebootplaygt@gmail.com>
2022-11-05 18:28:00 -0400

    Defined: continous_regex? which tells a passed list of arguments whether the regex is continous or not

--- src/rugrep.rb
+++ src/rugrep.rb
@@ -28,6 +28,29 @@ def parse_regex(exp)
   end
 end
 
+def continous_regex?(args)
+  # This function verifies if all the regex arguments
+  # conforms with the format specified.
+  # Return true if it is, false otherwise.
+  # First tries to find the first regex index using .find.
+  # if result is nil, return false. Then if it is true, it checks two elements ahead
+  # to check if there is any P, X, P args. And return false if it did. Then lastly ret true, if it didn't
+  # Assumes args >= 2, is checked in main
+  
+  first_regex_pos = args.length.times.find {|i| regex_format?(args[i])}
+
+  if not first_regex_pos
+    return false
+  end
+  
+  (first_regex_pos...(args.length - 2)).each do |i|
+    if not regex_format?(args[i + 1]) and regex_format?(args[i + 2])
+      return false
+    end
+  end
+  return true
+end
+
 def parseArgs(args)
   # Fill me
 end
