5a3fc08e116e79dfc4ee9cd1d2532df3b0aa3c47
Tamaarine <irebootplaygt@gmail.com>
2022-11-07 10:09:56 -0500

    Changed: get_regexs test suites to also remove the side-effect

--- tests/rugrep_spec.rb
+++ tests/rugrep_spec.rb
@@ -359,34 +359,38 @@ describe "Func: get_files" do
 end
 
 describe "Func: get_regexs" do
-  before :each do
-    $script_ret = ""
-  end
-
   it "Should return an empty list" do
     input = ["hehe", "file/example.txt", "files/html.html"]
-    expect(get_regexs(input)).to eq []
-    expect($script_ret).to eq ""
+    script_ret = ""
+    ret, script_ret = get_regexs(input, script_ret)
+    expect(ret).to eq []
+    expect(script_ret).to eq ""
   end
   
   it "Should return matching list" do
     input = ["hehe", "file/example.txt", "\"[az]pple\"","files/html.html"]
+    script_ret = ""
+    ret, script_ret = get_regexs(input, script_ret)
     exp = [/[az]pple/]
-    expect(get_regexs(input)).to eq exp
-    expect($script_ret).to eq ""
+    expect(ret).to eq exp
+    expect(script_ret).to eq ""
   end
   
   it "Should return matching list with matching script_ret" do
     input = ["hehe", "file/example.txt", "\"[az]pple\"", "\"[\"","files/html.html"]
+    script_ret = ""
+    ret, script_ret = get_regexs(input, script_ret)
     exp = [/[az]pple/]
-    expect(get_regexs(input)).to eq exp
-    expect($script_ret).to eq "Error: cannot parse regex \"[\"\n"
+    expect(ret).to eq exp
+    expect(script_ret).to eq "Error: cannot parse regex \"[\"\n"
   end
   
   it "Should return matching list with matching script_ret" do
     input = ["hehe", "file/example.txt", "\"[az]pple\"", "\"[\"", "\"?\"","files/html.html"]
+    script_ret = ""
+    ret, script_ret = get_regexs(input, script_ret)
     exp = [/[az]pple/]
-    expect(get_regexs(input)).to eq exp
-    expect($script_ret).to eq "Error: cannot parse regex \"[\"\nError: cannot parse regex \"?\"\n"
+    expect(ret).to eq exp
+    expect(script_ret).to eq "Error: cannot parse regex \"[\"\nError: cannot parse regex \"?\"\n"
   end
 end
